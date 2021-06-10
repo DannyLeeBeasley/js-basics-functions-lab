0c92da2a6314c2ea0ff526f4a0bea4f3703442d4
Dan Beasley <dannyleebeasley@gmail.com>
2021-06-10 09:35:03 -0500

    second submission

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-// Code your solution in this file!
-function distanceFromHqInBlocks(pickUpBlock){
-    return Math.abs(pickUpBlock-42, 10);
+const distanceFromHqInBlocks = (pickupBlock) => {
+    return Math.abs(pickupBlock - 42)
+} 
+
+const distanceFromHqInFeet = (pickupBlock) => {
+    return Math.abs(pickupBlock - 42) * 264
 }
 
-function distanceFromHqInFeet(pickUpBlock){
-    return distanceFromHqInBlocks(pickUpBlock)*264
+const distanceTravelledInFeet = (pickupBlock, dropoffBlock) => {
+    return Math.abs(pickupBlock - dropoffBlock) * 264
 }
 
-function distanceTravelledInFeet(pickUpBlock, dropOffBlock){
-    return Math.abs(pickUpBlock-dropOffBlock)*264
-}
+const calculatesFarePrice = (pickupBlock, dropoffBlock) => {
+    const tripDistanceInFeet = distanceTravelledInFeet(pickupBlock, dropoffBlock)
 
-function calculatesFarePrice(pickUpBlock, dropOffBlock){
-    const tripDistanceInFeet = distanceTravelledInFeet(pickUpBlock, dropOffBlock)
-    
     if (tripDistanceInFeet < 400){
         return 0
-    } 
+    }
     else if (tripDistanceInFeet > 400 && tripDistanceInFeet < 2000){
-        return (tripDistanceInFeet-400)*.02
+        return (tripDistanceInFeet - 400) * .02
     }
     else if (tripDistanceInFeet > 2000 && tripDistanceInFeet < 2500){
         return 25
     }
-    else if (tripDistanceInFeet > 2500){
-        return 'cannot travel that far'
-    }
+    else {return 'cannot travel that far'}
 }
